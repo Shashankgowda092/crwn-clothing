039a9ae1d1f4e644618c55c70d9d43ad9fd26b4a
Shashankgowda092 <65107083+Shashankgowda092@users.noreply.github.com>
2020-12-13 17:40:27 +0530

    added redux and working

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+
+
+
+
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store} >
+    <BrowserRouter>
      <App />
-  </BrowserRouter>,
+    </BrowserRouter>
+
+  </Provider>,
   document.getElementById('root')
 );
 
